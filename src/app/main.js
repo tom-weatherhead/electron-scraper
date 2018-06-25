db6d239faec15a7ab694744d0199af994c6ddde8
Tom Weatherhead <tom_weatherhead@hotmail.com>
2018-06-25 04:48:53 -0600

    Used settings.js

--- src/app/main.js
+++ src/app/main.js
@@ -47,51 +47,7 @@ ipc.on('invokeAction', function(event, data){
 });
 */
 
-const defaultSettings = {
-	descriptors: [
-		{
-			url: 'https://nodejs.org/en/',
-			regexes: [
-				/Download v{0,1}(\S+)\s+Current/
-			],
-			mapRegexIndexToDatabaseTableColumnName: [],
-			options: {
-				returnHttpResponseStatus: true
-			},
-			label: 'Current version of Node.js :',
-			stringificationTemplate: [
-				0
-			]
-		}
-	],
-	timerIntervalInMilliseconds: 60000,
-	logSuccessfulHttpRequests: false
-};
-
-/*
-function constructHttpErrorCodeTestSettings (httpStatusCode) {
-	return {
-		descriptors: [
-			{
-				url: `https://httpbin.org/status/${httpStatusCode}`,
-				regexes: defaultSettings.descriptors[0].regexes,
-				mapRegexIndexToDatabaseTableColumnName: defaultSettings.descriptors[0].mapRegexIndexToDatabaseTableColumnName,
-				options: defaultSettings.descriptors[0].options,
-				label: defaultSettings.descriptors[0].label,
-				stringificationTemplate: defaultSettings.descriptors[0].stringificationTemplate
-			}
-		],
-		timerIntervalInMilliseconds: defaultSettings.timerIntervalInMilliseconds,
-		logSuccessfulHttpRequests: defaultSettings.logSuccessfulHttpRequests
-	};
-}
-*/
-
-const settings = require('../../settings/settings.json') || defaultSettings;
-//const settings = require('../../settings/settings.example.json') || defaultSettings;
-//const settings = defaultSettings;
-//const settings = constructHttpErrorCodeTestSettings(500);
-//const settings = constructHttpErrorCodeTestSettings(403);
+const settings = require('../settings.js');
 
 ipcMain.on('getSettings', function (event) {
 	event.sender.send('actionReply_getSettings', settings);
--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,49 @@
 // electron-scraper/src/settings.js
 
-module.exports = {
+const defaultSettings = {
+	descriptors: [
+		{
+			url: 'https://nodejs.org/en/',
+			regexes: [
+				/Download v{0,1}(\S+)\s+Current/
+			],
+			mapRegexIndexToDatabaseTableColumnName: [],
+			options: {
+				returnHttpResponseStatus: true
+			},
+			label: 'Current version of Node.js :',
+			stringificationTemplate: [
+				0
+			]
+		}
+	],
+	timerIntervalInMilliseconds: 60000,
+	logSuccessfulHttpRequests: false
 };
+
+/*
+function constructHttpErrorCodeTestSettings (httpStatusCode) {
+	return {
+		descriptors: [
+			{
+				url: `https://httpbin.org/status/${httpStatusCode}`,
+				regexes: defaultSettings.descriptors[0].regexes,
+				mapRegexIndexToDatabaseTableColumnName: defaultSettings.descriptors[0].mapRegexIndexToDatabaseTableColumnName,
+				options: defaultSettings.descriptors[0].options,
+				label: defaultSettings.descriptors[0].label,
+				stringificationTemplate: defaultSettings.descriptors[0].stringificationTemplate
+			}
+		],
+		timerIntervalInMilliseconds: defaultSettings.timerIntervalInMilliseconds,
+		logSuccessfulHttpRequests: defaultSettings.logSuccessfulHttpRequests
+	};
+}
+*/
+
+const settings = require('../settings/settings.json') || defaultSettings;
+//const settings = require('../settings/settings.example.json') || defaultSettings;
+//const settings = defaultSettings;
+//const settings = constructHttpErrorCodeTestSettings(500);
+//const settings = constructHttpErrorCodeTestSettings(403);
+
+module.exports = settings;
